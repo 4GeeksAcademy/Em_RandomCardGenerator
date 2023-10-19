ed0f364c87c1c3198e5b1f704370d69e5d33014e
Emdihoy <136965828+Emdihoy@users.noreply.github.com>
2023-10-19 19:19:42 +0000

    Proyecto terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,54 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Numeros y letras aleatorias
+  function RandomCardNumber() {
+    let arr = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let randomNumber = arr[Math.floor(Math.random() * arr.length)];
+    return randomNumber;
+  }
+  let numeroRandomDeLaCarta = document.getElementById("cardNumber");
+  numeroRandomDeLaCarta.innerHTML = RandomCardNumber();
+
+  // Palos aleatorios
+
+  function randomIconGenerator() {
+    let arrIcon = ["♦", "♥", "♠", "♣"];
+    let randomIcon = arrIcon[Math.floor(Math.random() * arrIcon.length)];
+    return randomIcon;
+  }
+  let iconoRandom = randomIconGenerator();
+
+  let paloSuperior = document.querySelectorAll("#paloSup")[0];
+  paloSuperior.innerHTML = iconoRandom;
+
+  let paloInferior = document.querySelectorAll("#paloInf")[0];
+  paloInferior.innerHTML = iconoRandom;
+
+  //Generando color en el palo de la carta
+
+  paloSuperior.innerHTML = iconoRandom;
+  paloInferior.innerHTML = iconoRandom;
+
+  if (iconoRandom === "♦" || iconoRandom === "♥") {
+    paloSuperior.setAttribute("style", "color:red");
+    paloInferior.setAttribute("style", "color:red");
+  } else {
+    paloSuperior.setAttribute("style", "color:black");
+    paloInferior.setAttribute("style", "color:black");
+  }
 };
